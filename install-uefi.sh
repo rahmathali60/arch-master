7276ccef634d75712f660491c0f9c09a17d2abe2
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-02-14 16:11:55 +0530

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -10,12 +10,12 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:0985 | chpasswd
+echo root:alone | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet libvirt dialog wpa_supplicant mtools dosfstools reflector base-devel linux-lts-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils  alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp  bridge-utils dnsmasq vi ipset  flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr bluez bluez-utils networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-lts-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils  pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call-lts bridge-utils dnsmasq ipset sof-firmware nss-mdns acpid os-prober ntfs-3g 
 
 # pacman -S --noconfirm xf86-video-amdgpu
 #pacman -S --noconfirm nvidia-lts nvidia-utils nvidia-settings
@@ -26,16 +26,16 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
-useradd -m alone
-echo alone:0985 | chpasswd
-usermod -aG libvirt alone
+useradd -m feroz
+echo feroz:alone | chpasswd
+#usermod -aG libvirt feroz
 
-echo "alone ALL=(ALL) ALL" >> /etc/sudoers.d/alone
+echo "feroz ALL=(ALL) ALL" >> /etc/sudoers.d/feroz
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
