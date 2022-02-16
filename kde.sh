a4aaaed5647b73251a6f6aeb346764e82cc1778d
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-02-16 06:44:58 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -4,13 +4,12 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
 #sudo reflector -c Switzerland -a 6 --sort rate --save /etc/pacman.d/mirrorlist
-
+sudo pacman -Syy
 
 #sudo systemctl enable --now auto-cpufreq
-
-
-sudo pacman -S --noconfirm sddm plasma plasma-wayland-session ark gzip p7zip bzip2 dolphin dolphin-plugins konsole kate okular gwenview  kcalc kdeconnect gnome-icon-theme gnome-themes-extra firefox htop vlc powertop adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-sans-pro-fonts  
+sudo pacman -S sddm plasma simplescreenrecorder plasma-wayland-session ark dolphin dolphin-plugins konsole kate okular gwenview kcalc gnome-icon-theme gnome-themes-extra firefox htop vlc powertop 
+//gzip p7zip bzip2
 sudo systemctl enable sddm
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-reboot
+#/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+#sleep 5
+#reboot
