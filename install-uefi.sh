1feb7a8509e9b4c99e3ffabc5aba1728616bf19d
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-03-23 00:53:31 +0530

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -2,20 +2,20 @@
 
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
-locale-gen
+#sed -i '177s/.//' /etc/locale.gen
+#locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 #echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:kde | chpasswd
+echo root:0985 | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 pacman -Syy
-pacman -S grub efibootmgr bluez-qt bluez-utils networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-lts-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call-lts bridge-utils dnsmasq ipset sof-firmware nss-mdns acpid os-prober ntfs-3g 
+pacman -S grub efibootmgr bluez bluez-utils networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion acpi acpi_call bridge-utils dnsmasq ipset sof-firmware nss-mdns acpid os-prober ntfs-3g 
 
 # pacman -S --noconfirm xf86-video-amdgpu
 #pacman -S --noconfirm nvidia-lts nvidia-utils nvidia-settings
@@ -24,22 +24,18 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
-systemctl enable sshd
+#systemctl enable sshd
 systemctl enable avahi-daemon
 #systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-#systemctl enable reflector.timer
+systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
 useradd -m rahmath
-echo rahmath:kde | chpasswd
+echo rahmath:0985 | chpasswd
 #usermod -aG libvirt rahmath
 
 echo "rahmath ALL=(ALL) ALL" >> /etc/sudoers.d/rahmath
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
-
-
