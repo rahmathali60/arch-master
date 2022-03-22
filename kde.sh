3e5bc84fe1c52d3f5783d021f4f3c888f093636c
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-03-23 00:57:22 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -7,8 +7,8 @@ sudo hwclock --systohc
 sudo pacman -Syy
 
 #sudo systemctl enable --now auto-cpufreq
-sudo pacman -S sddm plasma simplescreenrecorder plasma-wayland-session ark dolphin dolphin-plugins konsole kate okular gwenview kcalc gnome-icon-theme gnome-themes-extra firefox htop vlc powertop 
-//gzip p7zip bzip2
+sudo pacman -S sddm plasma simplescreenrecorder plasma-wayland-session ark dolphin dolphin-plugins konsole kate okular gwenview kcalc gnome-icon-theme gnome-themes-extra htop vlc powertop 
+#gzip p7zip bzip2
 sudo systemctl enable sddm
 #/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 #sleep 5
