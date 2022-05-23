425f851a7283abed3ecafd400dec32336673b711
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-05-23 22:27:40 +0530

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -9,23 +9,25 @@ sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin/
 makepkg -si --noconfirm
 
-#pikaur -S --noconfirm system76-power
-#sudo systemctl enable --now system76-power
+paru -S  zramd
+sudo vim /etc/default/zramd
+sudo systemctl enable zramd.service --now
+
+#sudo systemctl enable --now system76- power
 #sudo system76-power graphics integrated
 #pikaur -S --noconfirm gnome-shell-extension-system76-power-git
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
-sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
-
-sudo flatpak install -y spotify
-sudo flatpak install -y kdenlive
+sudo pacman -S xfce4 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox simplescreenrecorder vlc 	thunar-archive-plugin 	xfce4-screenshooter 	mousepad 	xfce4-pulseaudio-plugin 	xfce4-notifyd 	xfce4-cpufreq-plugin 	ristretto 	xfce4-whiskermenu-plugin 	xfce4-battery-plugin 	parole 	xfce4-datetime-plugin 	xfce4-wavelan-plugin  	xfce4-dict 	xfce4-mpc-plugin  	thunar-media-tags-plugin
+#sudo flatpak install -y spotify
+#sudo flatpak install -y kdenlive
 
 sudo systemctl enable lightdm
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-sudo reboot
+#/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+#sleep 5
+#sudo reboot
